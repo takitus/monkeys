300daf7478600eb0f446a61e87a90f617813db9b
takitus <takitus@gmail.com>
2020-12-30 21:15:00 -0500

    Create aeroinstock.js

---
+++ aero/aeroinstock.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Aero In Stock
+// @namespace    http://takitus.net/
+// @version      0.1
+// @description  Displays only in stock items when choosing the 'show all' count on the page
+// @author       takitus
+// @match        https://www.aeroprecisionusa.com/*?product_list_limit=all
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    var $ = window.jQuery;
+    $('.unavailable').closest('.item').hide();
+})();
