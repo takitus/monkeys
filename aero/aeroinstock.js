51270ec13bf3bb794c6b676b68c2757483939d1a
takitus <takitus@gmail.com>
2020-12-30 21:17:18 -0500

    Update aeroinstock.js
    addded update location

--- aero/aeroinstock.js
+++ aero/aeroinstock.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         Aero In Stock
 // @namespace    http://takitus.net/
-// @version      0.1
+// @version      0.11
+// @updateURL    https://github.com/takitus/monkeys/blob/main/aero/aeroinstock.js
 // @description  Displays only in stock items when choosing the 'show all' count on the page
 // @author       takitus
 // @match        https://www.aeroprecisionusa.com/*?product_list_limit=all
