849268710481c52826e5d9ecb9d4bb422a8f051d
takitus <takitus@gmail.com>
2021-02-10 14:52:19 -0500

    Update natchezzsort.user.js

--- natchezz/natchezzsort.user.js
+++ natchezz/natchezzsort.user.js
@@ -2,7 +2,7 @@
 // @name         Natchezz List Sort
 // @namespace    http://takitus.net/
 // @version      0.11
-// @downloadURL  https://github.com/takitus/monkeys/raw/main/natchezz/natchezzsort.user.js
+// @downloadURL  https://github.com/takitus/monkeys/raw/main/natchez/natchezsort.user.js
 // @updateURL    https://github.com/takitus/monkeys/raw/main/natchez/natchezsort.user.js
 // @description  Moves all in stock items to the top of the list, hides extra info for out of stock
 // @author       takitus
